0a061ebb0836b3541394c756b09ee63072571570
Rogerio Homen <rogeriofhomen@gmail.com>
2022-04-09 09:58:11 -0300

    comentarios

--- controle.py
+++ controle.py
@@ -1,8 +1,7 @@
-from sqlite3 import Cursor
-from MySQLdb import apilevel
-from PyQt6 import  uic, QtWidgets
-import mysql.connector
+from PyQt6 import  uic, QtWidgets #importação e exibição da tabela feita no Qt Designer
+import mysql.connector #comunicação com MySQL
 
+#acesso ao banco de dados especifico. 
 banco = mysql.connector.connect(
     host="localhost",
     user="root",
@@ -10,55 +9,51 @@ banco = mysql.connector.connect(
     database="cadastro_produtos"
 )
 
+#função para executar quando apertar o botão "enviar".
 def funcao_principal():
     linha1 = formulario.lineEdit.text()
-    linha2 = formulario.lineEdit_2.text()
-    linha3 = formulario.lineEdit_3.text()
+    linha2 = formulario.lineEdit_2.text()   #variaveis que recebem os dados nas caixas de texto
+    linha3 = formulario.lineEdit_3.text()   
     categoria = ''
-    print('teste')
-    print(linha1, linha2, linha3)
-
+   #condicções para funcionar as opcoes de categoria no formulario
     if formulario.radioButton.isChecked():
-        print('Categoria: informática')
         categoria = 'informática'
     elif formulario.radioButton_2.isChecked():
-        print('Categoria: Alimentos')
         categoria = 'Alimentos'
     else:
-        print('Categoria: Eletrônicos')
         categoria = 'Eletrônicos'
-
-    cursor  =  banco.cursor ()
-    comando_SQL  =  "INSERT INTO produtos (codigo, descricao, preco, categoria) VALUE (%s,%s,%s,%s)"
+    #Para salvar dados no banco de dados selecionado(Criei uma variavel pra executar o codigo sql "comando_SQL")
+    cursor  =  banco.cursor() #serve para comunicar com o banco de dados 
+    comando_SQL  =  "INSERT INTO produtos (codigo, descricao, preco, categoria) VALUE (%s,%s,%s,%s)" 
     dados  = (str(linha1), str(linha2), str(linha3), categoria)
-    cursor.execute(comando_SQL, dados)
-    banco.commit()
+    cursor.execute(comando_SQL, dados) #executa os comandos (colunas, valor)
+    banco.commit()#salva no banco
+    #limpa as LineEdit após apertar o botão enviar(ainda dentro da função)
     formulario.lineEdit.setText('')
     formulario.lineEdit_2.setText('')
     formulario.lineEdit_3.setText('')
-
+#função que chama segunda tela com lista de produtos apos apertar o botao "visualizar"
 def visu_segunda_tela():
     segunda_tela.show()
     cursor = banco.cursor()
     comando_SQL = 'SELECT * FROM produtos'
     cursor.execute(comando_SQL)
-    dados_lidos = cursor.fetchall()
-    print(dados_lidos)
-
-    segunda_tela.tableWidget.setRowCount(len(dados_lidos))
-    segunda_tela.tableWidget.setColumnCount(5)
+    dados_lidos = cursor.fetchall()    
+    #cria uma tabela de exibição para segunda tela
+    segunda_tela.tableWidget.setRowCount(len(dados_lidos))#quantidade de linhas de acordo com a quatidade de dados lidos
+    segunda_tela.tableWidget.setColumnCount(5)#quantidade de colunas pré definida
         
     for i in range(0, len(dados_lidos)):
         for j in range(0, 5):
              segunda_tela.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j]))) 
              
 
-apl = QtWidgets.QApplication([])
-formulario = uic.loadUi("cadastroprodutos.ui")
-segunda_tela = uic.loadUi("visualizar.ui")
-formulario.pushButton.clicked.connect(funcao_principal)
-formulario.pushButton_2tela.clicked.connect(visu_segunda_tela)
+apl = QtWidgets.QApplication([]) #cria o aplicativo 
+formulario = uic.loadUi("cadastroprodutos.ui")#lê e carrega arquivo 'ui' criado pelo Qt Designer
+segunda_tela = uic.loadUi("visualizar.ui") #lê e carrega o aquivo 'ui' da segunda tela
+formulario.pushButton.clicked.connect(funcao_principal) #chama a funcção quando aperta o botão 'enviar'.
+formulario.pushButton_2tela.clicked.connect(visu_segunda_tela) #chama a função quando aperta o botão 'visualizar'
 
 
-formulario.show()
-apl.exec()
+formulario.show() #mostra a primeira tela com o arquivo "ui".
+apl.exec() #axecuta o aplicativo
